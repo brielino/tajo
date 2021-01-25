5c1b4b0233fe89b884dfa580739f521136de6ce7
brielino <61842589+brielino@users.noreply.github.com>
2021-01-25 18:15:46 +0100

    Update TestTextDatum.java

--- tajo-common/src/test/java/org/apache/tajo/common/TestTextDatum.java
+++ tajo-common/src/test/java/org/apache/tajo/common/TestTextDatum.java
@@ -20,29 +20,26 @@ public class TestTextDatum {
 	@Test
 	 public void equalsToTest() {
 		
-		for(int i =0;i<3;i++) {
-			switch (i) {
-			case 0:
-				Datum datum1 = (Datum) parametri[0][0];
-				Datum datum2 = (Datum) parametri[0][1];
-				Datum date = datum1.equalsTo(datum2);
-				Assert.assertEquals(true,date.asBool());
-			case 1:
-				Datum datum3 = (Datum) parametri[1][0];
-				Datum datum4 = (Datum) parametri[1][1];
-				Datum date1 = datum3.equalsTo(datum4);
-				Assert.assertEquals(false,date1.asBool());
-				
-			case 2:
-				Datum datum5 = (Datum) parametri[2][0];
-				Datum datum6 = (Datum) parametri[2][1];
-				try {
-					Datum date3 = datum5.equalsTo(datum6);
-		        } catch (Exception e) {
-		            Assert.assertEquals(NullPointerException.class, e.getClass());
-		        }
-			}	
-		}
+		
+		Datum datum1 = (Datum) parametri[0][0];
+		Datum datum2 = (Datum) parametri[0][1];
+		Datum date = datum1.equalsTo(datum2);
+		Assert.assertEquals(true,date.asBool());
+	
+		Datum datum3 = (Datum) parametri[1][0];
+		Datum datum4 = (Datum) parametri[1][1];
+		Datum date1 = datum3.equalsTo(datum4);
+		Assert.assertEquals(false,date1.asBool());
+		
+	
+		Datum datum5 = (Datum) parametri[2][0];
+		Datum datum6 = (Datum) parametri[2][1];
+		try {
+			Datum date3 = datum5.equalsTo(datum6);
+        } catch (Exception e) {
+            Assert.assertEquals(NullPointerException.class, e.getClass());
+        }
+		
 		//scritto per incrementare coverage
 		Datum datum = (Datum) parametri[0][0];
 		Datum null_datum = DatumFactory.createNullDatum();
@@ -61,57 +58,51 @@ public class TestTextDatum {
 	@Test
 	 public void equalsTest() {
 		
-		for(int i =0;i<3;i++) {
-			switch (i) {
-			case 0:
-				Datum datum1 = (Datum) parametri[0][0];
-				Datum datum2 = (Datum) parametri[0][1];
-				Boolean result1 = datum1.equals(datum2);
-				Assert.assertEquals(true,result1);
-			case 1:
-				Datum datum3 = (Datum) parametri[1][0];
-				Datum datum4 = (Datum) parametri[1][1];
-				Boolean result2 = datum3.equals(datum4);
-				Assert.assertEquals(false,result2);
-				
-			case 2:
-				Datum datum5 = (Datum) parametri[2][0];
-				Datum datum6 = (Datum) parametri[2][1];
-				try {
-					Boolean result3 = datum5.equals(datum6);
-		        } catch (Exception e) {
-		            Assert.assertEquals(NullPointerException.class, e.getClass());
-		        }
-			}	
-		}
+		
+		Datum datum1 = (Datum) parametri[0][0];
+		Datum datum2 = (Datum) parametri[0][1];
+		Boolean result1 = datum1.equals(datum2);
+		Assert.assertEquals(true,result1);
+	
+		Datum datum3 = (Datum) parametri[1][0];
+		Datum datum4 = (Datum) parametri[1][1];
+		Boolean result2 = datum3.equals(datum4);
+		Assert.assertEquals(false,result2);
+		
+	
+		Datum datum5 = (Datum) parametri[2][0];
+		Datum datum6 = (Datum) parametri[2][1];
+		try {
+			Boolean result3 = datum5.equals(datum6);
+        } catch (Exception e) {
+            Assert.assertEquals(NullPointerException.class, e.getClass());
+        }
+		
 	}
 	
 	@Test
 	 public void compareToTest() {
 		
-		for(int i =0;i<3;i++) {
-			switch (i) {
-			case 0:
-				Datum datum1 = (Datum) parametri[0][0];
-				Datum datum2 = (Datum) parametri[0][1];
-				int result1 = datum1.compareTo(datum2);
-				Assert.assertEquals(0,result1);
-			case 1:
-				Datum datum3 = (Datum) parametri[1][0];
-				Datum datum4 = (Datum) parametri[1][1];
-				int result2 = datum3.compareTo(datum4);
-				Assert.assertEquals(1,result2);
-				
-			case 2:
-				Datum datum5 = (Datum) parametri[2][0];
-				Datum datum6 = (Datum) parametri[2][1];
-				try {
-					int result3 = datum5.compareTo(datum6);
-		        } catch (Exception e) {
-		            Assert.assertEquals(NullPointerException.class, e.getClass());
-		        }
-			}	
-		}
+		
+		Datum datum1 = (Datum) parametri[0][0];
+		Datum datum2 = (Datum) parametri[0][1];
+		int result1 = datum1.compareTo(datum2);
+		Assert.assertEquals(0,result1);
+	
+		Datum datum3 = (Datum) parametri[1][0];
+		Datum datum4 = (Datum) parametri[1][1];
+		int result2 = datum3.compareTo(datum4);
+		Assert.assertEquals(1,result2);
+		
+	
+		Datum datum5 = (Datum) parametri[2][0];
+		Datum datum6 = (Datum) parametri[2][1];
+		try {
+			int result3 = datum5.compareTo(datum6);
+        } catch (Exception e) {
+            Assert.assertEquals(NullPointerException.class, e.getClass());
+        }
+		
 		//scritto per incrementare coverage
 		Datum datum = (Datum) parametri[0][0];
 		Datum null_datum = DatumFactory.createNullDatum();
@@ -149,5 +140,11 @@ public class TestTextDatum {
 	public void CoverageAsByte() {
 		TextDatum time = new TextDatum("11:00:00");
 		time.asByte();
+	}
+	@Test
+	public void MutationTest() {
+		Datum datum1 = (Datum) parametri[0][0];
+		boolean result = datum1.equals(new Object());
+		Assert.assertEquals(false,result);
 	}
 }
